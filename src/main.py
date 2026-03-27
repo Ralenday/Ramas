3b08fb950903c8dde980521cd4696f612cd3c7d1
Ralebday <axelcardenas557@gmail.com>
2026-03-26 20:55:53 -0700

    Feature: Modulo de productos implementado

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 Arc[D[D[D#[C[C[D[DA[Crchivo random para practica toda chida
 
 print()[D""[DSistema base cargado - Version Inicial")
+# --- Módulo de Productos ---
+def gestionar_productos():
+    print("📦 Módulo de Productos: Inventario y catálogo")
+gestionar_productos()[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[o[[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[B[B[D[D[D[D[D[D[D[D[o[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[a[B
