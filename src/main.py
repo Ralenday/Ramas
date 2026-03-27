27e7e7e685d965879efd97436383e93e9b352ecd
Ralebday <axelcardenas557@gmail.com>
2026-03-26 20:53:54 -0700

    Feature: Modulo de usuarios implementado

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 Arc[D[D[D#[C[C[D[DA[Crchivo random para practica toda chida
 
 print()[D""[DSistema base cargado - Version Inicial")
+Modulo de usuarios[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D#[C[C[C[C[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+def gestion_usuario():
+	print()[D""[DModulo de usuarios: CRUD completado")
+gestion_usuario()
